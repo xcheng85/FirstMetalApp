7b50ec7aaa0187139ada71b2da68a2653b32f2a0
xiao cheng <xcheng4@xiaos-MacBook-Pro.local>
2019-01-13 10:15:16 -0600

    Initial Commit

---
+++ FirstMetalApp/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  FirstMetalApp
+//
+//  Created by xiao cheng on 1/13/19.
+//  Copyright © 2019 xiao cheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
